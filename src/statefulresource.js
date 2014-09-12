eb9e70bd7ec56fbccf13784528968d27e349e94b
Marcelo Kanzaki <marcelojk@gmail.com>
2014-09-12 18:26:14 -0300

    introduces a bindable public params property

--- src/statefulresource.js
+++ src/statefulresource.js
@@ -1,22 +1,21 @@
 angular.module('statefulresource', [])
 
 .factory('StatefulResource', function($http) {
-  var _queryParams = {}
-
   var _statefulParams = function(paramsToMergeAndStore) {
-    return angular.extend(_queryParams, paramsToMergeAndStore)
+    return angular.extend(this.params, paramsToMergeAndStore)
   }
 
   var StatefulResource = function(endpoint, options) {
     this.models = []
     this.endpoint = endpoint
+    this.params = {}
     this.options = angular.extend({}, options)
   }
 
   StatefulResource.prototype.query = function(params) {
     var self = this
 
-    $http.get(this.endpoint, {params: _statefulParams(params)})
+    $http.get(this.endpoint, {params: _statefulParams.call(this, params)})
     .success(function(data) {
       self.models = data
     })
